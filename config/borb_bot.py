75a4dee66bddb9e989beefc8a681bfe2d442c571
Mike Tung <miketung2013@gmail.com>
2025-02-08 10:15:48 -0500

    refactored the way env vars were being brought into the configs
    - added util get_config function to handle env var setting
    - refactored configs to use get_config

--- config/borb_bot.py
+++ config/borb_bot.py
@@ -1,26 +1,13 @@
-import os
 import discord
 
-DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+from .util import get_config
 
-if not DISCORD_BOT_TOKEN:
-    raise AttributeError(f'DISCORD_BOT_TOKEN has not been set!')
-
-BOT_PREFIX = os.environ.get('BOT_PREFIX', '$')
+DISCORD_BOT_TOKEN = get_config(env_var='DISCORD_BOT_TOKEN')
+BOT_PREFIX = get_config(env_var='BOT_PREFIX', required=False, default_val='$')
+BOT_ROLE_MESSAGE_ID = get_config(env_var='ROLE_MESSAGE_ID', cast_var=int)
+BOT_MOD_AUDIT_CHANNEL_ID = get_config('MOD_AUDIT_CHANNEL_ID', cast_var=int)
+MODERATOR_ROLES = get_config('MODERATOR_ROLES').split(',')
 
 BOT_INTENTS = discord.Intents.default()
 BOT_INTENTS.members = True
 BOT_INTENTS.message_content = True
-
-BOT_ROLE_MESSAGE_ID = int(os.environ.get('ROLE_MESSAGE_ID'))
-BOT_MOD_AUDIT_CHANNEL_ID = int(os.environ.get('MOD_AUDIT_CHANNEL_ID'))
-
-if not BOT_ROLE_MESSAGE_ID:
-    raise AttributeError(f'BOT_ROLE_MESSAGE_ID cannot be null')
-if not BOT_MOD_AUDIT_CHANNEL_ID:
-    raise AttributeError(f'BOT_MOD_AUDIT_CHANNEL_ID cannot be null')
-
-MODERATOR_ROLES = os.environ.get('MODERATOR_ROLES').split(',')
-
-if not MODERATOR_ROLES:
-    raise AttributeError(f'MODERATOR_ROLES is not set!')
